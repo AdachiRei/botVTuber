8f4a8adf22ebc5f7108be772adae47b1a3ca2c01
TopStick <53611560+TopStick@users.noreply.github.com>
2019-08-10 17:55:21 +0900

    Update Program.cs

--- MarkovInu-master/MarkovInu/MarkovInu/Program.cs
+++ MarkovInu-master/MarkovInu/MarkovInu/Program.cs
@@ -14,11 +14,10 @@ using System.Linq;
 
 namespace MarkovInu
 {
-    // コメントを保存していくファイルのディレクトリ
-
-
+    
     internal class Program
     {
+        // コメントを保存していくファイルのディレクトリ
         const string path = @"C:\botVTuber\sample.txt";   //ここにsample.txtのpass
 
         // 形態素解析ライブラリのディレクトリ
@@ -52,12 +51,11 @@ namespace MarkovInu
             _meCabTagger = MeCabTagger.Create(MeCabParam);
             var markovDic = new MarkovDictionary();
 
-            // 正規表現(パターン検索)・・・この場合<>で囲まれた箇所にマッチする(+後述のreg.Replaceで文字を消す？)
+            // 正規表現(パターン検索)・・・この場合<>で囲まれた箇所にマッチする(+後述のreg.Replaceで文字を消す)
             var reg = new Regex(@"<.*?>");
+            
 
-
-
-            // 読み込む行数をなんとなく先に定義
+            // 読み込む行数を先に定義
             var n = 10;
 
             // lock構文の準備
@@ -65,16 +63,11 @@ namespace MarkovInu
 
             lock (lockReed)
             {
-                // ここから先はコメントの読み込みの方法を色々試した努力の痕跡
-                using (var file = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)) //var file = new StreamReader(path)
+                using (var file = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
                 {
                     var lines = File.ReadAllLines(path);
                     string line;
-                    //long pos = lines.Length - n;
-                    //file.Seek(pos, SeekOrigin.Begin);
-                    //while ((line = file.ReadLine() != null)
                     foreach (var ln in lines.Skip(lines.Length - n).Take(n))  // (最後の行-n行)目に跳んで、n行分読み込む
-                    //for (int i = line.Length - 20; i < line.Length; i++)
                     {
                         line = reg.Replace(ln, "");
                         var marList = CheckMeCab(line);
